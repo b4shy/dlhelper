270d2c81c96bb19719c90f325818307aff25cd51
Marcel Frueh <Marcel.Frueh@student.uni-tuebingen.de>
2019-03-31 12:54:29 +0200

    Expanded tests

--- read.py
+++ read.py
@@ -7,16 +7,21 @@ logger = logging.getLogger("READ")
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
-def read_images(path, img_no, size=(224, 224)):
-    logger.info("Read %i images from %s",img_no, path)
+def read_images(path, img_no=-1, size=(224, 224)):
+
+    if img_no < 0:
+        logger.info("Negative image number: Imread all images from %s", path)
+    else:
+        logger.info("Read %i images from %s", img_no, path)
     imgs = []
+
     for counter, i in enumerate(os.listdir(path)):
         img_path = os.path.join(path, i)
         img = cv2.imread(img_path)
         img = cv2.resize(img, size)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         imgs.append(img)
-        if counter == img_no:
+        if counter == img_no-1:
             break
     return np.array(imgs)
 
--- test/test_read.py
+++ test/test_read.py
@@ -1,8 +1,29 @@
 import pytest
+import os
 from context import read
 
 
-def test_read():
-    imgs = read.read_images("data",1)
+def test_read_up_to():
+    imgs = read.read_images("data", 1)
     assert len(imgs) == 1
+
+
+def test_read_all_imgs():
+    imgs = read.read_images("data")
+    assert len(imgs) == len(os.listdir("data"))
+
+
+def test_read_all_imgs_negative_img_number():
+    imgs = read.read_images("data",-2)
+    assert len(imgs) == len(os.listdir("data"))
+
+
+def test_read_all_imgs_2():
+    imgs = read.read_images("data",2)
+    assert len(imgs) == 2
+
+
+def test_read_all_imgs_with_size():
+    imgs = read.read_images("data", size=(299,299))
+    assert imgs.shape[1:-1] == (299,299)
 
