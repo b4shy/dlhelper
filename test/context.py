a80f9e033684bb1f1a4fb64baaaea8c070469139
Marcel <MarcelFrueh@gmx.de>
2019-03-31 17:00:08 +0200

    Structure

--- test/context.py
+++ test/context.py
@@ -2,7 +2,7 @@ import os
 import sys
 myPath = os.path.dirname(os.path.abspath(__file__))
 print(myPath)
-sys.path.insert(0, myPath + '/../imhelper')
+sys.path.insert(0, myPath + '/../')
 
 import read
 import show
