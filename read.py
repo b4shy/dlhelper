e0d7f45dcd2d5ddd9e779b720f0a57b9dac59345
Marcel <MarcelFrueh@gmx.de>
2019-03-31 17:17:36 +0200

    More tests

--- read.py
+++ read.py
@@ -12,15 +12,16 @@ def read_images(path, img_no=-1, size=(224, 224)):
     if img_no < 0:
         logger.info("Imread all images from %s", path)
     else:
-        logger.info("Read %i images from %s", img_no, path)
+        logger.info("Read %i image(s) from %s", img_no, path)
     imgs = []
 
     for counter, i in enumerate(os.listdir(path)):
 
         if counter == img_no:
+            logger.info("Done")
             break
 
-        if i.endswith("png") or i.endswith("jpg"):
+        if ".png" in i or ".jpg" in i:
             img_path = os.path.join(path, i)
             img = cv2.imread(img_path)
             img = cv2.resize(img, size)
@@ -28,7 +29,8 @@ def read_images(path, img_no=-1, size=(224, 224)):
             imgs.append(img)
 
         else:
-            logger.error("%s is not a valid image file",i)
+            logger.warning("%s is not a valid image file", i)
+            raise TypeError("{} is not a valid image file".format(i))
 
     return np.array(imgs)
 
--- test/test_read.py
+++ test/test_read.py
@@ -17,16 +17,20 @@ def test_read_all_imgs():
 
 
 def test_read_all_imgs_negative_img_number():
-    imgs = read.read_images(path,-2)
+    imgs = read.read_images(path, -2)
     assert len(imgs) == len(os.listdir(path))
 
 
 def test_read_all_imgs_2():
-    imgs = read.read_images(path,2)
+    imgs = read.read_images(path, 2)
     assert len(imgs) == 2
 
 
 def test_read_all_imgs_with_size():
-    imgs = read.read_images(path, size=(299,299))
-    assert imgs.shape[1:-1] == (299,299)
+    imgs = read.read_images(path, size=(299, 299))
+    assert imgs.shape[1:-1] == (299, 299)
 
+
+def test_wrong_file_name():
+    with pytest.raises(TypeError):
+        read.read_images(".")
