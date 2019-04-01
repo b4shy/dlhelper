d66b57e5cb4ff0b00e89455cf1f1da313e24234f
Marcel Frueh <Marcel.Frueh@student.uni-tuebingen.de>
2019-04-01 11:15:43 +0200

    Works

--- imhelper/structure.py
+++ imhelper/structure.py
@@ -4,8 +4,9 @@ import shutil
 class KerasFormatHelper:
     def __init__(self, path_to_image_folder, name_of_img_folder):
         self.path = path_to_image_folder
+        self.img_path = os.path.join(self.path, name_of_img_folder)
+        self.files = os.listdir(self.img_path)
         self.class_list = []
-        self.files = os.listdir(os.path.join(self.path, name_of_img_folder))
 
     def make_directories(self, class_list):
         self.class_list = class_list
@@ -16,7 +17,7 @@ class KerasFormatHelper:
         for img in self.files:
             for c, i in enumerate(format_string):
                 if i in img:
-                    shutil.copy(os.path.join(self.path, img), os.path.join(self.path, self.class_list[c]))
+                    shutil.copy(os.path.join(self.img_path, img), os.path.join(self.path, self.class_list[c]))
 
     def _mkdir(self, dirname):
         path_to_dir = os.path.join(self.path, dirname)
